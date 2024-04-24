1b2f17218eec16524cc32ad4b633f4879b5b944d
pictus <2379489+pictus@users.noreply.github.com>
2024-04-24 22:47:02 +0200

    adopted docker files

--- cli/src/enviroment.js
+++ cli/src/enviroment.js
@@ -1,7 +1,7 @@
 export const defaultConfig = {
-    APP_NAME: 'my-viktor-project',
-    VICTOR_ROOT: './.viktor/',
-    DOCKERFILE: './viktor/data/docker-compose.yml',
+    APP_NAME: 'viktor-project',
+    VICTOR_ROOT: './.viktor/viktor/',
+    DOCKERFILE: './viktor/viktor/data/docker-compose.yml',
     CONTAINER: 'server',
     SERVER_PORT_UNSECURE: 8080,
     SERVER_PORT_SECURE: 4443,
@@ -9,7 +9,6 @@ export const defaultConfig = {
     MERCURE_ENABLE: '1',
     MERCURE_JWT_KEY: '', // defaulted by random key in setupQuestions
     // for internal usage in the prompts    
-    // APP_DIR: '../',
     INT_PORTS: true,
 };
 export const PROJECT_DIR_NAME='.viktor'
